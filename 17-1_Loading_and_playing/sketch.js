7f92dc1eedc0007ca17b940b895e7f048419b9c7
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-24 18:31:23 -0300

    inicio

--- 17-1_Loading_and_playing/sketch.js
+++ 17-1_Loading_and_playing/sketch.js
@@ -1,43 +1,12 @@
-// erro no p5, verificar o que está acontecendo e ver se começa 
-// a funcionar direito como play/pause ao invés de play/stop
-
-let musica;
-let playing;
-let sliderVolume;
-let sliderRate;
-let sliderPan;
+let song;
 let button;
 
-function setup() {
-  createCanvas(200, 200);
 
-  musica = loadSound("bagulino.mp3", loaded);
+function setup() {
+  createCanvas(200,200);
+  song = loadSound('bagulino.mp3', loaded);
   button = createButton('play');
   button.mousePressed(tooglePlaying);
-
-  sliderVolume = createSlider(0, 1, 0.5, 0.01);
-  sliderRate = createSlider(0, 3, 1, 0.01);
-  sliderPan = createSlider(-1, 1, 0, 0.01);
-
+  background(51);
 }
 
-function loaded(){
-  console.log('loaded');
-}
-
-function tooglePlaying(){
-  if (!musica.isPlaying()) {
-    musica.play();
-    button.html('pause');
-  } else {
-    musica.pause();
-    button.html('play');
-  }
-}
-
-function draw() {
-  background(0);
-  musica.setVolume(sliderVolume.value());
-  musica.pan(sliderPan.value());
-  musica.rate(sliderRate.value());
-}
